4edbb329643b4811e332bf65ecd28e0255c16cd6
zwoong <zwoong6867@gmail.com>
2022-08-20 22:45:42 +0900

    modal ui completed

--- atoms/modalAtom.ts
+++ atoms/modalAtom.ts
@@ -1,11 +1,12 @@
 import { DocumentData } from 'firebase/firestore'
 import { atom } from 'recoil'
 import { Movie } from '../typings'
+import { v4 } from "uuid";
 
 // Atom은 상태(state)의 일부를 나타낸다. Atoms는 어떤 컴포넌트에서나 읽고 쓸 수 있다
 // 값을 읽는 컴포넌트들은 암묵적으로 Atom을 구독한다.
 export const modalState = atom({
-  key: 'modalState',
+  key: `modalState/${v4()}`,
   default: false,
 })
 
